8b5311b29496b88ac27453bc5c1e28bf3158f74f
Torio <jpstorio@mymail.mapua.edu.ph>
2023-05-24 04:17:31 +0800

    BR2 - fix deployment error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import Content from "./components/Content";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Content from "./Components/Content";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const client = new ApolloClient({
     uri: "https://graphql-pokemon2.vercel.app", // Replace with your GraphQL endpoint
