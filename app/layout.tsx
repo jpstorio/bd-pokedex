f28f1f2b5d1a9c51a2f490dc2ebeac831798100a
jpstorio <64604933+jpstorio@users.noreply.github.com>
2023-05-24 10:01:14 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Kanit } from "next/font/google";
 
-const kanit = Kanit({ weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
+const kanit = Kanit({ weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'] });
 
 export const metadata = {
     title: "BD - Pokedex",
